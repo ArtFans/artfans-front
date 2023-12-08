5462d537dcb145c141bac6026031a4d2e40cb57f
Педаль <@WIN-8CLN432C2UH>
2023-12-08 12:14:00 +0400

    Converting to ETH

--- pushback.sh
+++ pushback.sh
@@ -1,1 +1,13 @@
-rsync -av ./build/static ../artfans/public/static
+#!/bin/sh
+rm -r ../artfans/public/static
+rsync -av ./build/static ../artfans/public/
+cd ../artfans/public/static/js
+mv *.chunk.js chunk.js
+mv *.chunk.js.map chunk.js.map
+mv main.*.js main.js
+mv main.*.js.map main.js.map
+
+cd ../css
+mv main.*.css main.css
+mv main.*.css.map main.css.map
+
